f85787234b97518b8ec368096aa00610fe659ed1
Amirata K <amiratak88@gmail.com>
2018-07-09 23:44:27 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,29 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  arr = []
+  data.each do |k , v|
+    v.each do |k1, v1|
+      v1.each do |el|
+        arr << el
+      end
+    end
+  end
+  arr.uniq!
+  hash = {}
+  arr.each do |el|
+    hash[el] = {
+      :color => [],
+      :gender => [],
+      :lives => []
+    }
+  end
+  
+  data.each do |k , v|
+    v.each do |k1, v1|
+      v1.each do |el|
+        hash[el][k] << k1.to_s
+      end
+    end
+  end
+  hash
 end
