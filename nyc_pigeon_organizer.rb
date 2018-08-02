d3f51a2b0be7f291f123c28aea40eed0775cb7ea
Vadim Avnilov <vadim.avnilov@gmail.com>
2018-08-02 02:26:18 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  
+  data.each do |quality, properties|
+    properties.each do |property, pigeon_array|
+      pigeon_array.each do |pigeon_name|
+        if !pigeon_list[pigeon_name]
+          pigeon_list[pigeon_name] = {}
+        end
+        
+        if !pigeon_list[pigeon_name][quality]
+          pigeon_list[pigeon_name][quality] = []
+        end
+        pigeon_list[pigeon_name][quality] << property.to_s
+      end
+    end
+  end
+  pigeon_list
 end
